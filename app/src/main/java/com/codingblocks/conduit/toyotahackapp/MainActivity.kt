24fb2e24f3797657f3e8c4d6aac5bf0de784f8df
Arnav Gupta <arnav@codingblocks.com>
2018-12-17 16:04:10 +0530

    basic app
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ app/src/main/java/com/codingblocks/conduit/toyotahackapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.codingblocks.conduit.toyotahackapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnAdd.setOnClickListener {
+            val a = etVar1.text.toString().toInt()
+            val b = etVar2.text.toString().toInt()
+            val c = (a + b).toString()
+            tvResult.text = c
+        }
+
+
+    }
+}
